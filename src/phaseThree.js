aa4f1a1641bc7cc2fd225c1a7f5da17fb830df01
John Robson <j@hn.digital>
2021-11-12 12:44:26 -0500

    Face landmarks added

--- src/phaseThree.js
+++ src/phaseThree.js
@@ -1,5 +1,24 @@
 import { getStream } from './media'
 
+let videoLoaded = false
+
+const markWhiteList = [0, 1, 3]
+const drawFaceLandmarks = (predictions) => {
+    const dots = [...document.querySelectorAll('svg.landmarks circle')]
+    const marks = predictions[0]?.landmarks || []
+    const tl = predictions[0]?.topLeft || [0, 0]
+
+    for (let i in marks) {
+        if (!markWhiteList.includes(parseInt(i))) continue
+        const mark = marks[i]
+        dots[i].setAttribute('transform', `translate(${mark[0]}, ${mark[1]})`)
+        // dots[i].setAttribute('cx', mark[0])
+        // dots[i].setAttribute('cy', mark[1])
+
+        // console.log(dots[i], mark)
+    }
+}
+
 // Init phase 03
 export default async () => {
     // Get user media
@@ -11,11 +30,19 @@ export default async () => {
     // Set tracking class
     document.body.classList.add('tracking')
 
+    // Set viewbox to stream dims
+    const streamW = stream.getVideoTracks()[0].getSettings().width
+    const streamH = stream.getVideoTracks()[0].getSettings().height
+    const landmarkSVG = document.querySelector('svg.landmarks')
+    landmarkSVG.setAttribute('viewBox', `0 0 ${streamW} ${streamH}`)
+
     // Show video feed
     var video = document.querySelector('video.face-readout')
     video.srcObject = stream
-    video.onloadedmetadata = () => {
+    video.onloadedmetadata = async () => {
         video.play()
+        await new Promise((res) => setTimeout(res, 100))
+        videoLoaded = true
     }
 
     // Waveform visualizer
@@ -34,7 +61,9 @@ export default async () => {
     const WIDTH = waveCanvas.width
 
     function draw() {
-        requestAnimationFrame(draw)
+        if (document.body.classList.contains('tracking')) {
+            requestAnimationFrame(draw)
+        }
 
         analyser.getByteFrequencyData(dataArray)
 
@@ -57,6 +86,13 @@ export default async () => {
     draw()
 
     // canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    const model = await blazeface.load()
 
-    setInterval(() => {}, 500)
+    // Face detection loop
+    setInterval(async () => {
+        if (videoLoaded) {
+            const predictions = await model.estimateFaces(video)
+            drawFaceLandmarks(predictions)
+        }
+    }, 100)
 }
