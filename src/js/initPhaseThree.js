085c3e07cf31bbb23ff9c83968168393b8422b5c
John Robson <j@hn.digital>
2021-12-06 13:59:55 -0500

    Face landmarks are being detected and drawn to the SVG

--- src/js/initPhaseThree.js
+++ src/js/initPhaseThree.js
@@ -1,7 +1,13 @@
 import initAudioVisualizer from './initAudioVisualizer'
+import drawFaceLandmarks from './drawFaceLandmarks'
 import setNextPhrase from './setNextPhrase'
 import getStream from './getStream'
 import gameOver from './gameOver'
+
+const TF_FRAME_RATE = 10
+
+// Init vars
+let videoLoaded = false
 
 // Init phase 03
 export default async () => {
@@ -17,6 +23,8 @@ export default async () => {
 
     const streamW = stream.getVideoTracks()[0].getSettings().width
     const streamH = stream.getVideoTracks()[0].getSettings().height
+    const landmarkSVG = document.querySelector('svg.landmarks')
+    landmarkSVG.setAttribute('viewBox', `0 0 ${streamW} ${streamH}`)
 
     // Show video feed
     var video = document.querySelector('video.face-readout')
@@ -25,6 +33,8 @@ export default async () => {
     video.srcObject = stream
     video.onloadedmetadata = async () => {
         video.play()
+        await new Promise((res) => setTimeout(res, 100))
+        videoLoaded = true
     }
 
     // Display initial phrase
@@ -34,4 +44,23 @@ export default async () => {
     // Start drawing the waveforms of the stream
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
     const source = initAudioVisualizer(audioCtx, stream)
+
+    // Load the TensorFlow face detection
+    // model (this takes a second)
+    const model = await blazeface.load()
+
+    // Init face detection loop
+    let faceInterval = setInterval(async () => {
+        // make sure "tracking" class is still present
+        if (!document.body.classList.contains('tracking')) {
+            return clearInterval(faceInterval)
+        }
+
+        // class still there, if video is loaded...
+        if (videoLoaded) {
+            // draw landmarks to video
+            const predictions = await model.estimateFaces(video)
+            drawFaceLandmarks(predictions)
+        }
+    }, 1000 / TF_FRAME_RATE)
 }
